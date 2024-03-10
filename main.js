2bc3a72a98989b30a0e65d621f7ec4ba23f495bd
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-03-10 11:41:11 -0500

    added wimner function and button

--- main.js
+++ main.js
@@ -36,20 +36,30 @@ function render() {
 }
 
 function renderGame() {
-  gameBoard.forEach((colArr, colId) => {
-    colArr.forEach((cellVal, rowId) => {
-      const cellId = `c${colId}r${rowId}`;
+  gameBoard.forEach((rowArr, rowId) => {
+    rowArr.forEach((cellVal, colId) => {
+      const cellId = `r${rowId}c${colId}`;
       const cellEl = document.getElementById(cellId);
+      cellEl.rowId = rowId;
+      cellEl.colId = colId;
       cellEl.addEventListener("click", handleClick);
     });
   });
 }
-
 function handleClick(event) {
+  const col = event.target.colId;
+  const row = event.target.rowId;
   event.target.innerHTML = PLAYERS[turn];
+  gameBoard[row][col] = PLAYERS[turn];
+  console.log(gameBoard);
+  const hasWinner = checkWinner();
+  if (hasWinner) {
+    winner = turn;
+  }
   turn *= -1;
+
+  console.log(winner);
   renderMessage();
-  //   winner = checkWinner();
 }
 
 function renderMessage() {
@@ -61,10 +71,45 @@ function renderMessage() {
     message.innerHTML = `<span style="color:${PLAYERS[turn]}">${PLAYERS[turn]}</span>'s Turn!`;
   }
 }
+function checkWinner() {
+  return checkHorizontal() || checkVertical() || checkDiagonal();
+}
 
-//   4.2) Render those values to the page
-//   4.3) Wait for the user to click a square
+function checkVertical() {
+  for (let colId = 0; colId < gameBoard.length; colId++) {
+    if (
+      gameBoard[0][colId] != null &&
+      gameBoard[0][colId] === gameBoard[1][colId] &&
+      gameBoard[0][colId] === gameBoard[2][colId]
+    ) {
+      return true;
+    }
+  }
+}
 
-// 5) Handle a player clicking a square
+function checkHorizontal() {
+  for (let rowId = 0; rowId < gameBoard.length; rowId++) {
+    if (
+      gameBoard[rowId][0] != null &&
+      gameBoard[rowId][0] === gameBoard[rowId][1] &&
+      gameBoard[rowId][0] === gameBoard[rowId][2]
+    ) {
+      console.log("horizontal");
+      return true;
+    }
+  }
+}
 
-// 6) Handle a player clicking the replay button
+function checkDiagonal() {
+  if (
+    (gameBoard[0][0] != null &&
+      gameBoard[0][0] === gameBoard[1][1] &&
+      gameBoard[0][0] === gameBoard[2][2]) ||
+    (gameBoard[0][2] != null &&
+      gameBoard[0][2] === gameBoard[1][1] &&
+      gameBoard[0][2] === gameBoard[2][0])
+  ) {
+    console.log("diagonal");
+    return true;
+  }
+}
